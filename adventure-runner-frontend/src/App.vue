e738d6bfa4f25965d81eeae7d065e1ee9685a340
Paul Erpenstein <paul.erpenstein@gmail.com>
2022-08-16 15:16:03 +0200

    Icons and home link

--- adventure-runner-frontend/src/App.vue
+++ adventure-runner-frontend/src/App.vue
@@ -9,14 +9,27 @@ const getCurrentView = () => {
 </script>
 
 <template>
-  <header>
+  <header @click="route.home()">
+    <img class="logo" src="./assets/android-chrome-512x512.png" />
     <h1 class="site-title">Adventure runner</h1>
   </header>
 
   <main>
-    <component :is="getCurrentView()"/>
+    <component :is="getCurrentView()" />
   </main>
 </template>
 
 <style scoped>
+header {
+  padding: 8px;
+  display: flex;
+  align-items: center;
+  box-shadow: 0px 0px 6px #9d9d9d;
+}
+
+header .logo {
+  max-width: 50px;
+  max-height: 50px;
+  margin-right: 12px;
+}
 </style>
