b69a8f07412da8fbbf7a642ef204e2480a2af025
Paul Erpenstein <paul.erpenstein@gmail.com>
2022-08-18 10:07:22 +0200

    WIP style creature block

--- adventure-runner-backend/src/app.ts
+++ adventure-runner-backend/src/app.ts
@@ -2,9 +2,73 @@ import express from "express";
 import fsPromises from "fs.promises";
 import fs from "fs";
 import { marked } from 'marked';
+import yaml from "yaml";
+import hbs from "hbs";
 
 const app = express();
 const port = 3000;
+
+const crToXPMapping = {
+    "0": 10,
+    "1/8": 25,
+    "1/4": 50,
+    "1/2": 100,
+    "1": 200,
+    "2": 450,
+    "3": 700,
+    "4": 1100,
+    "5": 1800,
+    "6": 2300,
+    "7": 2900,
+    "8": 3900,
+    "9": 5000,
+    "10": 5900,
+    "11": 7200,
+    "12": 8400,
+    "13": 10000,
+    "14": 11500,
+    "15": 13000,
+    "16": 15000,
+    "17": 18000,
+    "18": 20000,
+    "19": 22000,
+    "20": 25000,
+    "21": 33000,
+    "22": 41000,
+    "23": 50000,
+    "24": 62000,
+    "30": 155000
+}
+app.set("views", __dirname + "/../views");
+
+hbs.registerHelper("ifEquals", function (arg1, arg2, options) {
+    return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
+});
+hbs.registerHelper("ifNotEquals", function (arg1, arg2, options) {
+    return (arg1 != arg2) ? options.fn(this) : options.inverse(this);
+});
+hbs.registerHelper("ifGreaterThan", function (arg1, arg2, options) {
+    return (arg1 > arg2) ? options.fn(this) : options.inverse(this);
+});
+hbs.registerHelper("eq", function (arg1, arg2) {
+    return arg1 == arg2;
+});
+hbs.registerHelper("capitalizeFirst", function (arg1) {
+    let str = ("" + arg1)
+    if (str.length > 0) {
+        return str.charAt(0).toUpperCase() + str.slice(1)
+    }
+    return str;
+});
+hbs.registerHelper("expByCr", function (arg1) {
+    return crToXPMapping[arg1]
+});
+hbs.registerHelper("breaklines", function (text) {
+    text = hbs.Utils.escapeExpression(text);
+    text = text.replace(/(\r\n|\n|\r)/gm, "<br>");
+    return new hbs.SafeString(text);
+});
+app.set('view engine', 'hbs');
 
 app.get("/api/tree", async (req, res, next) => {
     try {
@@ -16,9 +80,35 @@ app.get("/api/tree", async (req, res, next) => {
 });
 
 app.get("/api/content/*", async (req, res, next) => {
-    const fileContent = await fsPromises.readFile(`${__dirname}/../../content/${pathFromContent(decodeURI(req.url))}`);
-    res.send(marked.parse(fileContent.toString()));
+    switch (req.url.split(".").slice(-1)[0]) {
+        case "md":
+            const fileContent = await fsPromises.readFile(`${__dirname}/../../content/${pathFromContent(decodeURI(req.url))}`);
+            res.send(marked.parse(fileContent.toString()));
+            break;
+        default: res.send(await renderCreature(pathFromContent(req.url))); break;
+    }
 });
+
+const renderCreature = async (path) => {
+    const parsedFileContent = await getParsedFileContent(path);
+    return new Promise((resolve, reject) => {
+        app.render("statblock.hbs", parsedFileContent, (err, html) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(html);
+            }
+        });
+    });
+}
+
+const getParsedFileContent = async (path) => {
+    const fileContent = await fsPromises.readFile(`${__dirname}/../../content/${path}`);
+    switch (path.split(".").slice(-1)[0]) {
+        case "json": return JSON.parse(fileContent.toString())
+        case "yaml": return yaml.parse(fileContent.toString())
+    }
+}
 
 interface ContentTreeNode {
     name: string;
--- adventure-runner-frontend/src/components/ContentView.vue
+++ adventure-runner-frontend/src/components/ContentView.vue
@@ -16,5 +16,128 @@ onMounted(() => {
 </script>
 
 <template>
-    <div class="content" v-html="content"/>
+    <div class="content" v-html="content" />
 </template>
+
+<style lang="scss">
+.stat-block-wrapper {
+    width: 33vw;
+    margin: 20px;
+    font-family: 'Noto Sans', 'Myriad Pro', Calibri, Helvetica,
+        Arial, sans-serif;
+
+    .stat-block {
+        background: #FDF1DC;
+        padding: 5px 10px 20px;
+        box-shadow: 0 0 1.5em #867453;
+        margin-bottom: 15px;
+        border-top: #7A200D 3px solid;
+
+        .creature-heading {
+            font-family: 'Libre Baskerville', 'Lora', 'Calisto MT', 'Bookman Old Style', Bookman, 'Goudy Old Style', Garamond, 'Hoefler Text', 'Bitstream Charter', Georgia, serif;
+
+            h1 {
+                color: #922610;
+                font-size: 23px;
+                line-height: 1.2em;
+                margin: 10px 0 0;
+                letter-spacing: 1px;
+                font-variant: small-caps;
+                font-weight: bold;
+            }
+
+            h2 {
+                font-weight: normal;
+                font-style: italic;
+                font-size: 12px;
+                line-height: 1.2em;
+                margin: 0 0 10px;
+            }
+        }
+
+        .top-stats {
+            margin: 5px 0 8px 0;
+
+            .property-line {
+
+                h4,
+                p {
+                    display: inline;
+                    margin: 0;
+                    color: #922610;
+                    font-size: 13.5px;
+                    line-height: 1.2em;
+                }
+            }
+
+            .abilities-block {
+                display: flex;
+                flex-direction: row;
+                flex-wrap: nowrap;
+                justify-content: space-between;
+
+                .ability-score {
+                    display: flex;
+                    flex-direction: column;
+                    align-items: center;
+
+                    .ability-score-name {
+                        font-weight: bold;
+                        margin: 10px 0 2px;
+                        font-size: 14px;
+                        line-height: 1.2em;
+                        text-transform: uppercase;
+                        color: #7A200D;
+                    }
+
+                    .ability-score-mod,
+                    .ability-score-value {
+                        display: inline-block;
+                        color: #7A200D;
+                        margin: 0 0 10px;
+                        line-height: 1.2em;
+                        font-size: 13.5px;
+                    }
+                }
+            }
+        }
+
+        .property-block {
+            padding: 10px 2px 0;
+
+            h4,
+            p {
+                font-size: 13.5px;
+                line-height: 1.2em;
+                display: inline;
+                margin: 0;
+            }
+
+            h4 {
+                font-style: italic;
+            }
+        }
+
+        h3.actions {
+            border-bottom: 1px solid #7A200D;
+            color: #7A200D;
+            font-size: 21px;
+            font-variant: small-caps;
+            font-weight: normal;
+            letter-spacing: 1px;
+            margin: 20px 0 0;
+            padding: 0 0 10px;
+            text-indent: 5px;
+        }
+
+        .spacer {
+            height: 1px;
+            background-color: #7A200D;
+
+            &.top {
+                margin-top: 6px;
+            }
+        }
+    }
+}
+</style>
