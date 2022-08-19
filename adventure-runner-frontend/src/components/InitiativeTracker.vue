29fb6e88f99b70970d028cad15efeb14a7837620
Paul Erpenstein <paul.erpenstein@gmail.com>
2022-08-19 15:17:19 +0200

    Not working with reactivity

--- adventure-runner-frontend/src/components/InitiativeTracker.vue
+++ adventure-runner-frontend/src/components/InitiativeTracker.vue
@@ -2,29 +2,101 @@
 import { ref } from 'vue';
 import Draggable from "vue3-draggable";
 
+const newCreatureName = ref("");
+
 const trackedCreatures = ref([
     {
         name: "Blalala",
-        id: 1
+        id: 1,
+        initiative: 0,
+        hp: 20
     },
     {
         name: "12312",
-        id: 2
+        id: 2,
+        initiative: 0,
+        hp: 20
     },
     {
         name: "BlHelloHelloalala",
-        id: 3
+        id: 3,
+        initiative: 0,
+        hp: 20
     }
 ]);
+
+const addCreature = () => {
+    trackedCreatures.value.push({
+        name: newCreatureName.value,
+        id: trackedCreatures.value.length,
+        initiative: Math.max(...trackedCreatures.value.map(c => c.initiative)),
+        hp: 0
+    });
+
+    newCreatureName.value = "";
+}
+
+const deleteCreature = (id: number) => {
+    trackedCreatures.value = [...trackedCreatures.value.filter(c => c.id !== id)];
+}
 </script>
 
 <template>
-    <h1>Initiative Tracker</h1>
-    <draggable v-model="trackedCreatures">
-        <template v-slot:item="{ item }">
-            <div>
-                {{ item.name }}
-            </div>
-        </template>
-    </draggable>
+    <div class="initiative-tracker">
+        <h1>Initiative Tracker</h1>
+        <draggable v-model="trackedCreatures">
+            <template v-slot:item="{ item }">
+                <div class="creature-item">
+                    <input class="initiative-input" type="text" v-model="item.initiative" />
+                    &nbsp;
+                    {{ item.name }}
+
+                    <span class="delete" @click="deleteCreature(item.id)">X</span>
+                    <span class="hp-display">
+                        HP: <input class="hp-input" type="text" v-model="item.hp" />
+                    </span>
+
+                </div>
+            </template>
+        </draggable>
+        <span class="add-creature-holder">
+            <input type="text" v-model="newCreatureName"/>
+            <button @click="addCreature()">Add creature</button>
+        </span>
+    </div>
 </template>
+
+<style lang="scss">
+.initiative-tracker {
+    .creature-item {
+        padding: 5px 10px;
+        margin: 5px 0;
+        border: 1px grey solid;
+        border-radius: 5px;
+
+        .hp-display {
+            float: right;
+        }
+
+        .delete {
+            margin-left: 10px;
+            margin-top: 1px;
+            float: right;
+
+            &:hover {
+                color: red;
+            }
+        }
+
+        input {
+            text-align: center;
+            width: 30px;
+        }
+    }
+    .add-creature-holder {
+        button {
+            float: right;
+        }
+    }
+}
+</style>
